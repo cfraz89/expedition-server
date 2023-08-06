3e886d6c7003e2b12ea68e8e12bd811f1ba00d55
Chris Fraser <cfraz89@gmail.com>
2023-08-07 04:14:28 +1000

    Upload gpx files and convert to geojson

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,28 @@
-#![feature(once_cell)]
+mod net;
+
 use axum::{
+    extract::Multipart,
     http::StatusCode,
-    response::{IntoResponse, Response},
+    response::IntoResponse,
     routing::{get, post},
     Json, Router,
 };
+use geojson::GeoJson;
+use gpx::{Gpx, Waypoint};
+use net::response::{ResponseError, Result};
 use serde::{Deserialize, Serialize};
-use std::sync::OnceLock;
+use std::sync::{Arc, OnceLock};
 use surrealdb::{
     engine::remote::ws::{Client, Ws},
+    opt::auth::Root,
     Surreal,
 };
 
 static DB: OnceLock<Surreal<Client>> = OnceLock::new();
 
-enum AppError {
-    Status(StatusCode, &'static str),
-    Surreal(surrealdb::Error),
-    Any(color_eyre::eyre::Error),
-}
-
-impl IntoResponse for AppError {
-    fn into_response(self) -> Response {
-        match self {
-            AppError::Status(s, e) => (s, e).into_response(),
-            AppError::Any(e) => {
-                (StatusCode::INTERNAL_SERVER_ERROR, format!("Error: {}", e)).into_response()
-            }
-            AppError::Surreal(e) => (
-                StatusCode::SERVICE_UNAVAILABLE,
-                format!("Surreal error: {}", e),
-            )
-                .into_response(),
-        }
-    }
-}
-
-impl<E> From<E> for AppError
-where
-    E: Into<color_eyre::eyre::Error>,
-{
-    fn from(err: E) -> Self {
-        AppError::Any(err.into())
-    }
-}
-
-fn get_db() -> Result<&'static Surreal<Client>, AppError> {
-    match DB.get() {
-        None => Err(AppError::Status(
-            StatusCode::INTERNAL_SERVER_ERROR,
-            "Failed to get db",
-        )),
-        Some(db) => Ok(db),
-    }
+fn get_db() -> Result<&'static Surreal<Client>> {
+    DB.get()
+        .ok_or(ResponseError::internal_server_error("Failed to get db"))
 }
 
 #[tokio::main]
@@ -61,13 +31,19 @@ async fn main() -> color_eyre::Result<()> {
     // initialize tracing
     tracing_subscriber::fmt::init();
     let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
+    db.signin(Root {
+        username: "root",
+        password: "root",
+    })
+    .await?;
     db.use_ns("ausadv").use_db("ausadv").await?;
     DB.set(db).unwrap();
 
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/rides", get(get_rides));
+        .route("/rides", get(get_rides))
+        .route("/gpx", post(import_gpx));
 
     // run our app with hyper, listening globally on port 3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -80,11 +56,74 @@ async fn main() -> color_eyre::Result<()> {
 #[derive(Serialize, Deserialize)]
 struct Ride {
     name: String,
+    geo_json: GeoJson,
 }
 
-async fn get_rides() -> Result<Json<Vec<Ride>>, AppError> {
-    let rides = get_db()?.select("ride").await?;
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    Ok(Json(rides))
+async fn get_rides() -> Result<Json<Vec<serde_json::Value>>> {
+    let mut rides = get_db()?
+        .query("select meta::id(id) as id, name from rides")
+        .await?;
+    let ride_names: Vec<serde_json::Value> = rides.take(0)?;
+    Ok(Json(ride_names))
+}
+
+async fn import_gpx(mut multipart: Multipart) -> Result<impl IntoResponse> {
+    let mut geo_json_opt: Option<GeoJson> = None;
+    let mut ride_name_opt: Option<String> = None;
+    while let Some(field) = multipart.next_field().await? {
+        let name = field.name().ok_or(ResponseError::internal_server_error(
+            "No name on form field",
+        ))?;
+        match name {
+            "ride_name" => ride_name_opt = Some(field.text().await?),
+            "gpx" => {
+                let text = field.text().await?;
+                let gpx_data = gpx::read(text.as_bytes())?;
+                let gj: geojson::FeatureCollection = gpx_data
+                    .tracks
+                    .into_iter()
+                    .map(|track: gpx::Track| {
+                        // println!("{:?}", track);
+                        return geojson::Feature {
+                            geometry: Some(geojson::Geometry::new(geojson::Value::Polygon(
+                                track
+                                    .segments
+                                    .into_iter()
+                                    .map(|segment| {
+                                        segment
+                                            .points
+                                            .into_iter()
+                                            .map(|point: Waypoint| {
+                                                vec![point.point().x(), point.point().y()]
+                                            })
+                                            .collect::<Vec<Vec<f64>>>()
+                                    })
+                                    .collect::<Vec<Vec<Vec<f64>>>>(),
+                            ))),
+                            ..Default::default()
+                        };
+                    })
+                    .collect();
+                geo_json_opt = Some(GeoJson::FeatureCollection(gj));
+            }
+            _ => continue,
+        }
+    }
+    let ride_name = ride_name_opt.ok_or(ResponseError::with_status(
+        StatusCode::BAD_REQUEST,
+        "ride_name not provided",
+    ))?;
+    let geo_json = geo_json_opt.ok_or(ResponseError::with_status(
+        StatusCode::BAD_REQUEST,
+        "gpx not provided",
+    ))?;
+    let _ride: Ride = get_db()?
+        .create("rides")
+        .content(Ride {
+            name: ride_name,
+            geo_json,
+        })
+        .await?;
+
+    Ok("ok")
 }
---
+++ src/net/response.rs
@@ -0,0 +1,45 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+
+pub struct ResponseError(Response);
+
+impl IntoResponse for ResponseError {
+    fn into_response(self) -> Response {
+        self.0
+    }
+}
+
+impl<E> From<E> for ResponseError
+where
+    E: Into<color_eyre::eyre::Error>,
+{
+    fn from(value: E) -> Self {
+        Self(
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Into::<color_eyre::eyre::Error>::into(value).to_string(),
+            )
+                .into_response(),
+        )
+    }
+}
+
+impl ResponseError {
+    pub fn with_status<T>(status_code: StatusCode, data: T) -> Self
+    where
+        (StatusCode, T): IntoResponse,
+    {
+        ResponseError((status_code, data).into_response())
+    }
+
+    pub fn internal_server_error<T>(data: T) -> Self
+    where
+        (StatusCode, T): IntoResponse,
+    {
+        ResponseError((StatusCode::INTERNAL_SERVER_ERROR, data).into_response())
+    }
+}
+
+pub type Result<T, E = ResponseError> = axum::response::Result<T, E>;
