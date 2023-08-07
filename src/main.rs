83269d09b8592a958db603bff914eaa3dfeca61d
Chris Fraser <cfraz89@gmail.com>
2023-08-08 04:54:41 +1000

    Calculate bounding box, clean up conversion

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,19 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use geojson::GeoJson;
-use gpx::{Gpx, Waypoint};
+use geo_types::Coord;
+use geojson::{GeoJson, JsonObject};
+use gpx::{Gpx, TrackSegment, Waypoint};
 use net::response::{ResponseError, Result};
 use serde::{Deserialize, Serialize};
 use std::sync::{Arc, OnceLock};
 use surrealdb::{
     engine::remote::ws::{Client, Ws},
     opt::auth::Root,
+    sql::{Id, Thing},
     Surreal,
 };
+use tower_http::cors::CorsLayer;
 
 static DB: OnceLock<Surreal<Client>> = OnceLock::new();
 
@@ -43,7 +46,8 @@ async fn main() -> color_eyre::Result<()> {
     let app = Router::new()
         .route("/gpx", post(import_gpx))
         .route("/rides", get(get_rides))
-        .route("/rides/:id", get(get_ride_by_id));
+        .route("/rides/:id", get(get_ride_by_id))
+        .layer(CorsLayer::permissive());
 
     // run our app with hyper, listening globally on port 3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -55,9 +59,18 @@ async fn main() -> color_eyre::Result<()> {
 
 #[derive(Serialize, Deserialize)]
 struct Ride {
+    id: Option<Thing>,
     name: String,
     geo_json: GeoJson,
 }
+
+// struct RideGeo {
+//     geo_json: GeoJson,
+//     min_x: Option<f64>,
+//     min_y: Option<f64>,
+//     max_x: Option<f64>,
+//     max_y: Option<f64>,
+// }
 
 async fn get_rides() -> Result<Json<Vec<serde_json::Value>>> {
     let mut rides = get_db()?
@@ -85,32 +98,30 @@ async fn import_gpx(mut multipart: Multipart) -> Result<impl IntoResponse> {
             "gpx" => {
                 let text = field.text().await?;
                 let gpx_data = gpx::read(text.as_bytes())?;
-                let gj: geojson::FeatureCollection = gpx_data
+                let geo_json: geojson::GeoJson = gpx_data
                     .tracks
                     .into_iter()
                     .map(|track: gpx::Track| {
                         // println!("{:?}", track);
+                        let mls = track.multilinestring();
+                        let bbox = get_bounding_box(&mls);
+                        let center = bbox.clone().map(get_center);
+                        let mut properties = JsonObject::new();
+                        properties.insert(String::from("center"), center.into());
                         return geojson::Feature {
-                            geometry: Some(geojson::Geometry::new(geojson::Value::Polygon(
-                                track
-                                    .segments
-                                    .into_iter()
-                                    .map(|segment| {
-                                        segment
-                                            .points
-                                            .into_iter()
-                                            .map(|point: Waypoint| {
-                                                vec![point.point().x(), point.point().y()]
-                                            })
-                                            .collect::<Vec<Vec<f64>>>()
-                                    })
-                                    .collect::<Vec<Vec<Vec<f64>>>>(),
-                            ))),
+                            bbox: bbox.clone(),
+                            geometry: Some(geojson::Geometry {
+                                bbox: bbox,
+                                value: geojson::Value::from(&mls),
+                                foreign_members: None,
+                            }),
+                            properties: Some(properties),
                             ..Default::default()
                         };
                     })
-                    .collect();
-                geo_json_opt = Some(GeoJson::FeatureCollection(gj));
+                    .collect::<geojson::FeatureCollection>()
+                    .into();
+                geo_json_opt = Some(geo_json)
             }
             _ => continue,
         }
@@ -126,10 +137,55 @@ async fn import_gpx(mut multipart: Multipart) -> Result<impl IntoResponse> {
     let _ride: Ride = get_db()?
         .create("rides")
         .content(Ride {
+            id: None,
             name: ride_name,
             geo_json,
         })
         .await?;
 
     Ok("ok")
+}
+
+fn get_bounding_box(multi_line_string: &geo_types::MultiLineString) -> Option<Vec<f64>> {
+    let mut min_x: Option<&f64> = None;
+    let mut min_y: Option<&f64> = None;
+    let mut max_x: Option<&f64> = None;
+    let mut max_y: Option<&f64> = None;
+    for line_string in multi_line_string {
+        for Coord { x, y } in line_string.coords() {
+            min_x = match min_x {
+                None => Some(x),
+                Some(mx) if x < mx => Some(x),
+                Some(..) => min_x,
+            };
+            min_y = match min_y {
+                None => Some(y),
+                Some(my) if y < my => Some(y),
+                Some(..) => min_y,
+            };
+            max_x = match max_x {
+                None => Some(x),
+                Some(mx) if x > mx => Some(x),
+                Some(..) => max_x,
+            };
+            max_y = match max_y {
+                None => Some(y),
+                Some(my) if y > my => Some(y),
+                Some(..) => max_y,
+            };
+        }
+    }
+    match (min_x, min_y, max_x, max_y) {
+        (Some(min_x), Some(min_y), Some(max_x), Some(max_y)) => {
+            Some(vec![*min_x, *min_y, *max_x, *max_y])
+        }
+        _ => None,
+    }
+}
+
+fn get_center(bounding_box: Vec<f64>) -> Vec<f64> {
+    if let [min_x, min_y, max_x, max_y] = bounding_box[..] {
+        return vec![(max_x - min_x) / 2.0, (max_y - min_y) / 2.0];
+    };
+    return vec![0.0, 0.0];
 }
