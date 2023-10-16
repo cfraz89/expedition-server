13264e0a46a7334cb8c50626c95e1a211aebafd9
Chris Fraser <cfraz89@gmail.com>
2023-10-17 02:05:20 +1100

    rename expedition-server to expedition-backend

--- build-k3s.sh
+++ build-k3s.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -eux -o pipefail
-nerdctl build . -t expedition-server
-nerdctl save expedition-server | sudo nerdctl --address /var/run/k3s/containerd/containerd.sock --namespace k8s.io load
+nerdctl build . -t expedition-backend
+nerdctl save expedition-backend | sudo nerdctl --address /var/run/k3s/containerd/containerd.sock --namespace k8s.io load
